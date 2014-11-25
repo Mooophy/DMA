8659120c1fc489f3e29ef24730ec4b16231fe0e8
mooophy <MoophyAlan@gmail.com>
2014-11-25 23:34:42 +1300

    modified:   proj07.cpp done

--- ch01/function.hpp
+++ ch01/function.hpp
@@ -1,11 +1,25 @@
+//!
+//! @author Yue Wang
+//! @date   25.11.2014
+//!
+//! @brief  this code implemented a wrapper for function
+//!
+
 #ifndef FUNCTION_HPP
 #define FUNCTION_HPP
 
 #include <iterator>
 #include <algorithm>
+#include <set>
+#include <memory>
 
 namespace dma {
 
+/**
+ * @brief The Function class
+ *
+ * a function wrapper provides a few features
+ */
 template<typename Iter, typename Func>
 class Function
 {
@@ -20,9 +34,12 @@ public:
         func_{std::move(func)}
     {}
 
+    /**
+     * @brief is_one2one
+     */
     bool is_one2one()const
     {
-        return true;
+        return do_is_one2one();
     }
 
     bool is_invertible()const
@@ -40,12 +57,31 @@ public:
         return val;
     }
 
+    Function& reset(Func new_func)
+    {
+        func_ = std::move(new_func);
+        return *this;
+    }
 private:
-    Iter pre_begin_;
-    Iter pre_end_;
-    Iter img_begin_;
-    Iter img_end_;
+    const Iter pre_begin_;
+    const Iter pre_end_;
+    const Iter img_begin_;
+    const Iter img_end_;
     Func func_;
+
+    bool do_is_one2one()const
+    {
+        std::set<Value> set;
+        std::vector<Value> vec;
+        for(auto it = pre_begin_; it != pre_end_; ++it)
+        {
+            auto res = func_(*it);
+            set.insert(res);
+            vec.push_back(res);
+        }
+
+        return set.size() == vec.size();
+    }
 };
 
 }//namespace
--- ch01/proj07.cpp
+++ ch01/proj07.cpp
@@ -1,3 +1,11 @@
+//!
+//! @author Yue Wang
+//! @date   25.11.2014
+//!
+//! @brief  proj7, chapter 1.
+//!         check if one to one function
+//!
+
 #include <vector>
 #include <functional>
 #include "function.hpp"
@@ -5,12 +13,22 @@
 
 int main()
 {
+    //! init
     using Iter          =   typename    std::vector<int>::const_iterator;
     using FuncWrapper   =               dma::Function<Iter, std::function<int(int)> >;
+    auto func = [](int i){  return i * i; };
+    std::vector<int> v{-1, 1,2,3,4};
 
-    auto func = [](int i){  return i; };
-    std::vector<int> v{1,2,3,4};
-    FuncWrapper wrapper(v.cbegin(), v.cend(), v.cbegin(), v.cend(),func);
+    //! check the first function : func
+    FuncWrapper wrapper(v.cbegin(), v.cend(), v.cbegin(), v.cend(), func);
+    dma::print("func : ");
+    dma::println(wrapper.is_one2one()? "one to one !" : "not one to one.");
 
-    dma::println(wrapper.is_one2one());
+    //! check the second function : lambda
+    wrapper.reset([](int i){  return i * i; });
+    dma::print("this lambda : ") ;
+    dma::println(wrapper.is_one2one()? "one to one !" : "not one to one.");
+
+    dma::exit();
+    return 0;
 }
