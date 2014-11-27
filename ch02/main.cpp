19e45d3e5b63bceba112c954eb6d4acf1f65e879
mooophy <MoophyAlan@gmail.com>
2014-11-28 01:14:52 +1300

    modified:   main.cpp 	modified:   sort.hpp

--- ch02/main.cpp
+++ ch02/main.cpp
@@ -6,9 +6,9 @@
 
 int main()
 {
-    std::vector<int> v{5,6,3,7,2,8,1,9,12345};
+    std::vector<int> v{5,2,6,3,4,7,1};
     dma::println_range(v);
-    dma::bubble_sort(v.begin(),v.end());
+    dma::insertion_sort(v.begin(),v.end());
     dma::println_range(v);
 
     return 0;
--- ch02/sort.hpp
+++ ch02/sort.hpp
@@ -11,15 +11,34 @@
 
 namespace dma {
 
+/**
+ * @brief   bubble_sort
+ * @complx  O(n^2)
+ */
 template<typename It>
 void bubble_sort(It first, It last)
 {
     if(last-first < 2 ) return;
-
     using std::swap;
     for(auto i=first; i != last-1; ++i)
         for(auto j=first; j != first+(last-1-i); ++j)
             if(*j > *(j+1)) swap(*j, *(j+1));
+}
+
+/**
+ * @brief   insertion_sort
+ * @complx  O(n^2)
+ */
+template<typename It>
+void insertion_sort(It first, It last)
+{
+    if(last - first < 2)    return;
+    for(auto i = first+1; i != last; ++i){
+        auto key = *i;
+        auto j = i-1;
+        for(  ;  j != first-1  and  *j > key;  --j)    *(j+1)  =  *j;
+        *++j = key;
+    }
 }
 
 }//namespace
