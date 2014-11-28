c08c61134aa926a3bee634035a3761100dee08f8
mooophy <MoophyAlan@gmail.com>
2014-11-28 14:17:07 +1300

    modified:   ch02/ch02.py

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -4,9 +4,11 @@ algorithms from ch02 DMA
 '''
 
 #find the max element from the container
-def max(l):
-    max = l[0]
-    for elem in range(0,len(l)) : 
-        if(elem) > max : max = elem
+def max(seq):
+    max = seq[0]
+    for elem in seq: 
+        if elem > max : max = elem
     return max
-
+'''
+def linear_search(l,val):
+'''
