ceca84e2d8a2c4042b9474b22694bf1242c3c3c8
mooophy <MoophyAlan@gmail.com>
2014-11-28 15:46:57 +1300

    add binary search 	modified:   ch02.py

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -6,9 +6,33 @@ algorithms from ch02 DMA
 #find the max element from the container
 def max(seq):
     max = seq[0]
-    for elem in seq: 
-        if elem > max : max = elem
+    for elem in seq : 
+        if elem > max   :   max = elem
     return max
-'''
-def linear_search(l,val):
-'''
+
+#linear search
+def linear_search(seq,val):
+    for idx,elem in enumerate(seq):
+        if elem == val  :   return idx
+    return None
+
+def binary_search(seq,val):
+	beg = 0
+	end = len(seq)
+	while beg < end	:
+		mid = beg + (end - beg)//2
+		if seq[mid] == val:	
+			return mid
+		elif seq[mid] > val:	
+			end = mid
+		else:	
+			beg = mid
+	return None	
+
+
+
+		
+
+
+
+
