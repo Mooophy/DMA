750b5f255b81a31d55925fe8048b340d38fc1fe6
mooophy <MoophyAlan@gmail.com>
2014-11-28 14:02:51 +1300

    modified:   ch02/ch02.py

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -4,9 +4,9 @@ algorithms from ch02 DMA
 '''
 
 #find the max element from the container
-def max(list):
-    max = list[0]
-    for elem in range(0,len(list)) : 
+def max(l):
+    max = l[0]
+    for elem in range(0,len(l)) : 
         if(elem) > max : max = elem
     return max
 
