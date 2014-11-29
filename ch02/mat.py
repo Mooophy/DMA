c24b354a391e9379eeba36358c1ac7316f3286ca
mooophy <MoophyAlan@gmail.com>
2014-11-30 01:01:59 +1300

    new file:   mat.py

---
+++ ch02/mat.py
@@ -0,0 +1,16 @@
+'''
+algorithms from section 2.5, Rosen's DMA 5th
+    by Yue Wang 30.11.2014
+'''
+
+'''
+A simple matrix
+'''
+class Matrix:
+    def __init__(self,data):
+        self.data_ = data
+    def print(self):
+        for row in self.data_:
+            for elem in row:
+                print(elem, end=' ')
+            print()
