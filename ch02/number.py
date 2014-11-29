d6cb8283653d338507cab3b1760675e948500c9f
mooophy <MoophyAlan@gmail.com>
2014-11-29 14:52:55 +1300

    modified:   number.py

--- ch02/number.py
+++ ch02/number.py
@@ -4,16 +4,17 @@ algorithms from section 2.5, Rosen's DMA 5th
 '''
 
 '''
+Constructing Base b Expansions
 O( log base (num) )
 '''
 def get_expansion(num, base=2) :
     expansion = []
     while num>0 :
         expansion = [num%base] + expansion
-        num //= base 
+        num //= base
     return expansion
 
-''' 
+'''
 O(n)
 '''
 def add(lhs, rhs, base=2) :
@@ -21,13 +22,13 @@ def add(lhs, rhs, base=2) :
     r = get_expansion(rhs,base)
     size = max(len(l),len(r))
     l = [0] * (size-len(l)) + l
-    r = [0] * (size-len(r)) + r 
+    r = [0] * (size-len(r)) + r
     ret = []
     carry = 0
-    for i in reversed(range(size)) : 
+    for i in reversed(range(size)) :
         digit_sum = l[i] + r[i] + carry
-        ret = [digit_sum % base] + ret 
+        ret = [digit_sum % base] + ret
         carry = digit_sum//base
     if(carry>0):
         ret = [carry] + ret
-    return ret 
+    return ret
