4add71cb94ab14b7e1247b358b617aacdcb971bb
mooophy <MoophyAlan@gmail.com>
2014-12-03 02:54:25 +1300

    modified:   ch03/merge_sort.py

--- ch03/merge_sort.py
+++ ch03/merge_sort.py
@@ -1,7 +1,8 @@
 def merge(seq, first, mid, last):
     (left, right) = (seq[first: mid], seq[mid: last])
     (l, r,  curr) = (0, 0, first)
-    while l != len(left) and r != len(right):
+    (left_size, right_size) = (len(left), len(right))
+    while l != left_size and r != right_size:
         if left[l] < right[r]:
             seq[curr] = left[l]
             l += 1
@@ -9,7 +10,7 @@ def merge(seq, first, mid, last):
             seq[curr] = right[r]
             r += 1
         curr += 1
-    (x, rest) = (r, right) if l == len(left) else (l, left)
+    (x, rest) = (r, right) if l == left_size else (l, left)
     for idx in range(x, len(rest)):
         seq[curr] = rest[x]
         curr += 1
