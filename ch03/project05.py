774e82dfa910746482bc625c825a9a4a3895373a
mooophy <MoophyAlan@gmail.com>
2014-12-05 19:55:17 +1300

    modified:   ch03/project05.py

--- ch03/project05.py
+++ ch03/project05.py
@@ -1,3 +1,11 @@
+"""
+@author Yue Wang
+@date   05.12.2014
+@brief  Implemented for DMA
+@project 05 : generate well-formed formulae for operands : x,y,z and operators : + - * /
+"""
+
+
 def generate_formulae(operands, operators, max_size=2):
     expr = operands
     for size in range(max_size-1):
@@ -11,5 +19,8 @@ result = generate_formulae(exp, ops)
 print(result)
 
 
-
+"""
+@output:
+{'y', 'y-x', 'y+x', 'z-z', 'z*y', 'x*x', 'y+z', 'y*x', 'y-z', 'y+y', 'x+z', 'z/y', 'z/z', 'x-y', 'z+x', 'y/x', 'x*y', 'z+y', 'x-z', 'z*z', 'z', 'x/x', 'z-y', 'x+y', 'x*z', 'y/y', 'z/x', 'z*x', 'z-x', 'z+z', 'x/z', 'x/y', 'y/z', 'x+x', 'x', 'y*y', 'y*z', 'y-y', 'x-x'}
+"""
 
