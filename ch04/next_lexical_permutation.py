dd778476fcc7ce8f7b9a295821c341ccb0657401
mooophy <MoophyAlan@gmail.com>
2014-12-06 00:46:58 +1300

    modified:   ch04/next_lexical_permutation.py

--- ch04/next_lexical_permutation.py
+++ ch04/next_lexical_permutation.py
@@ -7,17 +7,17 @@
 """
 
 
-def next_permutation(a):
-    i = len(a) - 2
-    while not (i < 0 or a[i] < a[i+1]):
+def next_permutation(arr):
+    i = len(arr) - 2
+    while not (i < 0 or arr[i] < arr[i+1]):
         i -= 1
     if i < 0:
         return False
-    j = len(a) - 1
-    while not (a[j] > a[i]):
+    j = len(arr) - 1
+    while not (arr[j] > arr[i]):
         j -= 1
-    a[i], a[j] = a[j], a[i]        # swap
-    a[i+1:] = reversed(a[i+1:])    # reverse
+    arr[i], arr[j] = arr[j], arr[i]     # swap
+    arr[i+1:] = reversed(arr[i+1:])     # reverse
     return True
 
 
