83c8afcb0f329997864aae38b3fcc8812ab932fc
mooophy <MoophyAlan@gmail.com>
2014-12-12 12:05:20 +1300

    renamed:    NextLexicalBitStirng.hs -> nextLexicalBitStirng.hs

--- ch04/nextLexicalBitStirng.hs
+++ ch04/nextLexicalBitStirng.hs
@@ -5,4 +5,3 @@ nextLexicalBitStirng :: (Num a, Eq a) =>  [a] -> [a]
 nextLexicalBitStirng []     =   []
 nextLexicalBitStirng xs     =   if head rxs==0  then reverse(1:(tail rxs))  else nextLexicalBitStirng(take (length xs-1) xs) ++ [0]
                                 where rxs = reverse xs
-
