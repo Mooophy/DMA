34747acc2e4a707b3370538dff1a5fe8fe84b3ff
mooophy <MoophyAlan@gmail.com>
2014-12-12 11:52:58 +1300

    try on  haskell 	new file:   NextLexicalBitStirng.hs 	new file:   next_largest_bit_string.py

---
+++ ch04/NextLexicalBitStirng.hs
@@ -0,0 +1,8 @@
+{-  for ch04 DMA
+ -  by Yue Wang
+ - -}
+nextLexicalBitStirng :: (Num a, Eq a) =>  [a] -> [a]
+nextLexicalBitStirng []     =   []
+nextLexicalBitStirng xs     =   if head rxs==0  then reverse(1:(tail rxs))  else nextLexicalBitStirng(take (length xs-1) xs) ++ [0]
+                                where rxs = reverse xs
+
---
+++ ch04/next_largest_bit_string.py
@@ -0,0 +1,3 @@
+def next_largest_bit_string(bit_string):
+    for i in reversed(len(bit_string)):
+        if bit_string[i] ==
