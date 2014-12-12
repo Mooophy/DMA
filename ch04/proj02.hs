8488ee1ecd76d02b9e075d2dcb6db4457858fc85
mooophy <MoophyAlan@gmail.com>
2014-12-12 22:42:02 +1300

    modified:   proj02.hs

--- ch04/proj02.hs
+++ ch04/proj02.hs
@@ -4,6 +4,11 @@
  - -}
 
 
-rPermutation::(Integral a) => a -> a -> a 
-rPermutation n r = n^r
+rPermutationsRepetition::(Integral a) => a -> a -> a 
+rPermutationsRepetition n r = n^r
 
+
+--theorem 2 : C(n+r-1,r)    i.e. (n+r-1)!/r!*(n-1)!
+rCombinations :: (Fractional a, Eq a) =>  a -> a -> a
+rCombinations n r = fac(n+r-1)/((fac r) * fac (n-1))
+    where fac x = if x==0 then 1 else x * fac(x-1)
