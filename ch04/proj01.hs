a5bde759b7ba793e11c3437181161150de8a1e33
mooophy <MoophyAlan@gmail.com>
2014-12-12 22:03:19 +1300

    modified:   proj01.hs

--- ch04/proj01.hs
+++ ch04/proj01.hs
@@ -1,9 +1,8 @@
--- proj01 part I find the number of r-permutations
+-- proj01  find the number of r-permutations and of r-combinations
 rPermutations :: (Num a, Enum a) => a -> a -> a
 rPermutations n r = product (reverse [n-r+1..n])
--- proj01 part II find the number of r-combinations
--- problem : this code caused float output..need fix later on
-rCombinations :: (Fractional a, Eq a) =>  a -> a -> a
+
+rCombinations :: (Fractional a, Eq a) =>  a -> a -> a   -- this output float number, need to fix later on
 rCombinations n r = fac n/((fac r) * fac(n-r))
     where fac x = if x==0 then 1 else x * fac(x-1)
 
