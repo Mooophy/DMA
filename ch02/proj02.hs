3d874612b3f0c0ad6fa10be7a806faf37ca928c2
mooophy <MoophyAlan@gmail.com>
2014-12-13 19:08:29 +1300

    modified:   proj02.hs

--- ch02/proj02.hs
+++ ch02/proj02.hs
@@ -3,7 +3,7 @@
  - proj02 Given a list, find the first and last occurrences of the largest element.
  - -}
 firstAndLast :: (Ord a) => [a] -> (Int,Int)
-firstAndLast []         =   error "empty list"
-firstAndLast xs         =   (fstMax xs, lstMax xs)
+firstAndLast [] = error "empty list"
+firstAndLast xs = (fstMax xs, lstMax xs)
     where   fstMax seq@(y:ys) = if y        == maximum seq      then 0                  else 1 + fstMax ys
             lstMax seq@(y:ys) = if last seq == maximum seq      then length seq - 1     else lstMax (init seq)
