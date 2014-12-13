c27586826815232028f11643f1c87714803dbe22
mooophy <MoophyAlan@gmail.com>
2014-12-13 22:50:45 +1300

    modified:   proj04.hs

--- ch02/proj04.hs
+++ ch02/proj04.hs
@@ -3,10 +3,10 @@
  - proj04 binary search search.
  - -}
 binSearch :: (Ord a) => [a] -> a -> Int
-binSearch [] val        =   error "empty list"
-binSearch [x] val       =   if x == val then 0 else 1
-binSearch xs val
-    | xs !! mid == val  =   mid
-    | xs !! mid > val   =   0 +     binSearch (take mid xs) val
-    | otherwise         =   mid +   binSearch (drop mid xs) val 
+binSearch []  val  =  error "empty list"
+binSearch [x] val  =  if x == val then 0 else 1
+binSearch xs  val  =  case compare (xs !! mid) val of
+    EQ -> mid
+    GT -> 0   + binSearch (take mid xs) val
+    LT -> mid + binSearch (drop mid xs) val  
     where mid = div (length xs) 2
