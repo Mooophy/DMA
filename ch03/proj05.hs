204bef052209eaf29497d1dc697e89949b010afd
mooophy <MoophyAlan@gmail.com>
2014-12-14 22:57:45 +1300

    modified:   proj05.hs

--- ch03/proj05.hs
+++ ch03/proj05.hs
@@ -1,13 +1,21 @@
 --proj05 Generate well-formed formulae using : x,y,z and  + - * /
 generate :: [[Char]] -> [[Char]] -> Int -> [[Char]]
-generate [] _ _  =  throw
-generate _ [] _  =  throw
+generate [] _ _  =  error "wrong inputs"
+generate _ [] _  =  error "wrong inputs"
 generate es os n 
-    | n < 0      =  throw
+    | n < 0      =  error "wrong inputs"
     | otherwise  =  first (gen (es,os,n))
-    where   throw           =   error "wrong inputs" 
-            first(x,_,_)    =   x
+    where   first(x,_,_)    =   x
             gen (es,os,n) 
-                | n == 0    =   (es, os n)
+                | n == 0    =   (es, os, n)
                 | otherwise =   gen(nextEs, os, n-1)
-                where   nextEs  =  [l ++ o ++ r | l<-es, r<-es, o<-os]
+                where   nextEs  =  es ++ [l ++ o ++ r | l<-es, r<-es, o<-os]
+
+
+--test:
+es = ["x","y","z"]
+os = ["+","-","*","/"]
+main = do print (generate es os 1)
+--output:
+--["x","y","z","x+x","x-x","x*x","x/x","x+y","x-y","x*y","x/y","x+z","x-z","x*z","x/z","y+x","y-x","y*x","y/x","y+y","y-y","y*y","y/y","y+z","y-z","y*z","y/z","z+x","z-x","z*x","z/x","z+y","z-y","z*y","z/y","z+z","z-z","z*z","z/z"]
+--
