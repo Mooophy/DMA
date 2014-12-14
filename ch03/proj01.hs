877e5913a1008e0baa5559c3d76d0e3eada3f283
mooophy <MoophyAlan@gmail.com>
2014-12-14 19:17:01 +1300

    new file:   proj01.hs

---
+++ ch03/proj01.hs
@@ -0,0 +1,20 @@
+--proj01 sum
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+
+product' :: (Num a) => [a] -> a
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+
+--test:
+li = [1,2,3,4,5]
+main = do   print li
+            print (sum' li )
+            print (product' li) 
+--output:
+--[1,2,3,4,5]
+--15
+--120
