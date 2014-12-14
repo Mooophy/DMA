f4670655b7602d6a446405c970aa7573b44a3b12
mooophy <MoophyAlan@gmail.com>
2014-12-14 14:20:52 +1300

    modified:   proj07.hs

--- ch02/proj07.hs
+++ ch02/proj07.hs
@@ -5,14 +5,16 @@
 greedy::(Ord a, Num a) => [a] -> a -> [a]
 greedy [] _ = []
 greedy coins total = first (grd ([], coins, total))
-    where first (x,_,_)             =   x
-          grd (rets, coins, total)  =   if total==0 
-                                        then (rets, coins, total)
-                                        else grd (curr:rets, newCoins, newTotal)
-                                        where curr     = if isTooBig    then coins!!1             else head coins
-                                              newCoins = if isTooBig    then tail coins           else coins
-                                              newTotal = if isTooBig    then total - coins!!1     else total - coins!!0
-                                              isTooBig = head coins > total
+            where 
+                first (x,_,_)     =   x
+                grd (rets, coins, total)  
+                    | total==0        =   (rets, coins, total)
+                    | otherwise       =   grd (curr:rets, newCoins, newTotal)
+                    where 
+                        curr        =   if isBig    then coins!!1             else head coins
+                        newCoins    =   if isBig    then tail coins           else coins
+                        newTotal    =   if isBig    then total - coins!!1     else total - coins!!0
+                        isBig       =   head coins > total
           
 
 main = print (greedy [100,50,10,5,2,1] 189)
