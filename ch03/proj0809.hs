141ddd58cbdd9c2fefe9ad027a772e96ccfb9888
mooophy <MoophyAlan@gmail.com>
2014-12-15 00:40:10 +1300

    new file:   proj0809.hs

---
+++ ch03/proj0809.hs
@@ -0,0 +1,12 @@
+--proj08 power
+pow :: Integer -> Integer -> Integer
+pow a n = if n == 0 then 1 else a * pow a (n-1)
+
+--proj09
+proj09 a n = pow a (pow 2 n)
+
+main =  do  print (pow 10 2)
+            print (proj09 2 3)  
+--output :
+--100
+--256
