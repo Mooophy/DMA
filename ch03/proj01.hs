fd1627b838c6c54a43c7fec77149041ac31d41e7
mooophy <MoophyAlan@gmail.com>
2014-12-14 19:19:28 +1300

    modified:   proj01.hs

--- ch03/proj01.hs
+++ ch03/proj01.hs
@@ -1,4 +1,4 @@
---proj01 sum
+--proj01 sum and product
 sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
@@ -12,7 +12,7 @@ product' (x:xs) = x * product' xs
 --test:
 li = [1,2,3,4,5]
 main = do   print li
-            print (sum' li )
+            print (sum' li)
             print (product' li) 
 --output:
 --[1,2,3,4,5]
