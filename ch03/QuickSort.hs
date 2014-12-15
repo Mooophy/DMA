5f4226b951bd5a2b3286142952d0d115a0e985ae
mooophy <MoophyAlan@gmail.com>
2014-12-15 13:13:35 +1300

    modified:   QuickSort.hs

--- ch03/QuickSort.hs
+++ ch03/QuickSort.hs
@@ -9,8 +9,6 @@ quickSort (x:xs) = quickSort[y | y<-xs, y<=x] ++ [x] ++ quickSort[y | y<-xs, y>x
 li = [5,4,1,3,2]
 main = do print li
           print (quickSort li)
-          print (quickSort [])
 --output:
 --[5,4,1,3,2]
 --[1,2,3,4,5]
---[1]
